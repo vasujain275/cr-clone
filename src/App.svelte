daea2841d0e18ff1f65934756d0519b760c4e5f3
vasujain275 <vasujain275@gmail.com>
2024-05-16 23:13:28 +0530

    feat: NavBar Logo

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
-<div class="grid gap-4 m-4">
-  <div class="min-h-[100px] rounded bg-orange-500 shadow-xl"></div>
-  <div class="min-h-[100px] rounded bg-teal-500 shadow-xl"></div>
-</div>
-
+<nav class="grid sm:grid-cols-12 bg-sec min-h-[50px]">
+  <div class="sm:col-span-2 flex justify-end items-center">
+    <img class="w-30 h-7" src="cr.png" alt="crunchyroll" />
+  </div>
+  <div class="sm:col-span-8"></div>
+  <div class="sm:col-span-2"></div>
+</nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{svelte,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sec: "#1d2021",
+        pri: "#181a1b",
+      },
+    },
   },
   plugins: [],
-}
+};
